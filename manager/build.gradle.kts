c5f421fe1dd4a7fce2cf9d0cba1c199255c29f7d
ミドリ <midori01@users.noreply.github.com>
2026-03-08 06:27:39 +0900

    Update build.gradle.kts

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -11,7 +11,7 @@ val androidBuildToolsVersion by extra("36.1.0")
 val androidCompileNdkVersion by extra(libs.versions.ndk.get())
 val androidSourceCompatibility by extra(JavaVersion.VERSION_21)
 val androidTargetCompatibility by extra(JavaVersion.VERSION_21)
-val managerVersionCode by extra(30000 + getGitCommitCount() + 700)
+val managerVersionCode by extra(30000 + getGitCommitCount())
 val managerVersionName by extra(getGitDescribe())
 
 fun getGitCommitCount(): Int {
